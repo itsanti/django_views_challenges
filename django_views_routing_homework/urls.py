036c3513768243fd6f14eea4c60c00197628ebdf
Aleksandr <aleksandr.kurov@gmail.com>
2023-11-22 21:15:06 +0300

    Task d

--- django_views_routing_homework/urls.py
+++ django_views_routing_homework/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from django_views_routing_homework.views.level_1.a_welcome_user import welcome_user_view
 from django_views_routing_homework.views.level_1.b_bye_user import bye_user_view
 from django_views_routing_homework.views.level_1.c_baned_username import is_username_banned_view
+from django_views_routing_homework.views.level_1.d_user_info import get_user_info_view
 from django_views_routing_homework.views.level_2.a_user_info_by_username import get_user_info_by_username_view
 from django_views_routing_homework.views.level_2.c_product_type import get_products_view
 from django_views_routing_homework.views.level_2.d_authorization import authorization_view, process_authorization_view
@@ -18,6 +19,7 @@ urlpatterns = [
     path('welcome/', welcome_user_view),
     path('bye/', bye_user_view),
     path('banned/<slug:username>/', is_username_banned_view),
+    path('user-info/<int:user_id>/', get_user_info_view),
     path('user-info-by-username/<int:username>/', get_user_info_by_username_view),
     path('products/', get_products_view),
     path('authorization/', authorization_view),
