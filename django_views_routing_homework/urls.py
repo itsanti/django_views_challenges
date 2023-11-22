734fac6037162875dbf9e28ef2980f9c7a01b7f4
Aleksandr <aleksandr.kurov@gmail.com>
2023-11-22 21:04:09 +0300

    Task b

--- django_views_routing_homework/urls.py
+++ django_views_routing_homework/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 
 from django_views_routing_homework.views.level_1.a_welcome_user import welcome_user_view
+from django_views_routing_homework.views.level_1.b_bye_user import bye_user_view
 from django_views_routing_homework.views.level_1.c_baned_username import is_username_banned_view
 from django_views_routing_homework.views.level_2.a_user_info_by_username import get_user_info_by_username_view
 from django_views_routing_homework.views.level_2.c_product_type import get_products_view
@@ -15,6 +16,7 @@ from django_views_routing_homework.views.level_3.a_user_ip import show_user_ip_v
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('welcome/', welcome_user_view),
+    path('bye/', bye_user_view),
     path('banned/<slug:username>/', is_username_banned_view),
     path('user-info-by-username/<int:username>/', get_user_info_by_username_view),
     path('products/', get_products_view),
