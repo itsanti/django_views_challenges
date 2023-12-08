ef7f9172918e35b698272f6b6f3d8a9ee2e3f927
Aleksandr <kurov.aleksandr@gmail.com>
2023-12-09 01:13:50 +0300

    Solve level_3

--- django_views_routing_homework/views/level_3/b_validate_user_data.py
+++ django_views_routing_homework/views/level_3/b_validate_user_data.py
@@ -17,9 +17,55 @@
 Для тестирования рекомендую использовать Postman.
 Когда будете писать код, не забывайте о читаемости, поддерживаемости и модульности.
 """
+import json
+from django.http import HttpResponse, HttpRequest, JsonResponse, HttpResponseNotAllowed
+from http import HTTPStatus
+from typing import TypedDict
+from typing_extensions import NotRequired
+import enum
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 
-from django.http import HttpResponse, HttpRequest
+class RegisteredFrom(enum.Enum):
+    WEBSITE = 'website'
+    MOBILE_APP = 'mobile_app'
 
+    @classmethod
+    def has_value(cls, value):
+        return value in cls._value2member_map_
+
+
+class User(TypedDict):
+    full_name: str
+    email: str
+    registered_from: RegisteredFrom
+    age: NotRequired[int]
+
+
+def is_valid_user_data(user: User):
+    if (len(user.keys()) > 4 or
+            len(set(user.keys()).intersection(set(['full_name', 'email', 'registered_from']))) != 3):
+        return False
+    if any([type(user['full_name']) != str, type(user['email']) != str, type(user['registered_from']) != str]):
+        return False
+    if len(user['full_name']) < 5 or len(user['full_name']) > 256:
+        return False
+    try:
+        validate_email(user['email'])
+    except ValidationError as e:
+        return False
+    if not RegisteredFrom.has_value(user['registered_from']):
+        return False
+    if type(user.get('age', 0)) != int:
+        return False
+    return True
 
 def validate_user_data_view(request: HttpRequest) -> HttpResponse:
-    pass  # код писать тут
+    if request.method == 'POST':
+        try:
+            data: User = json.loads(request.body)
+        except json.decoder.JSONDecodeError:
+            return JsonResponse(data={}, status=HTTPStatus.BAD_REQUEST)
+        return JsonResponse(data={"is_valid": is_valid_user_data(data)})
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['POST'])
--- django_views_routing_homework/views/level_3/d_file_generation.py
+++ django_views_routing_homework/views/level_3/d_file_generation.py
@@ -13,9 +13,26 @@
 Для проверки используйте браузер: когда ручка правильно работает, при попытке зайти на неё, браузер должен
 скачивать сгенерированный файл.
 """
+import random
+import string
+from django.http import HttpResponse, HttpRequest, HttpResponseForbidden
 
-from django.http import HttpResponse, HttpRequest
 
+def text_generator(length):
+    return ''.join(random.choice(string.ascii_letters + ' ,.!?') for _ in range(length))
 
 def generate_file_with_text_view(request: HttpRequest) -> HttpResponse:
-    pass  # код писать тут
+    FILE_SIZE_LIMIT = (1, 1024)
+    length = request.GET.get('length', '')
+    try:
+        length = int(length)
+    except ValueError:
+        return HttpResponseForbidden()
+    if length < FILE_SIZE_LIMIT[0] or length > FILE_SIZE_LIMIT[1]:
+        return HttpResponseForbidden()
+
+    headers = {
+        "Content-Type": "text/plain",
+        "Content-Disposition": 'attachment; filename="generated.txt"'
+    }
+    return HttpResponse(text_generator(length), headers=headers)
